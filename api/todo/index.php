0a846b97928ce6716ce0d0dacf9139558e8dc8d6
marcocareddu <saldumaccu@tuta.io>
2023-07-20 15:12:28 +0200

    PHP - Moved Array from PHP to JSon

--- api/todo/index.php
+++ api/todo/index.php
@@ -1,34 +1,5 @@
 <?php
 
-// Moved array from JS
-$tasks = [
-    [
-        'text' => 'Comprare il pane',
-        'isDone' => false,
-        'id' => 0,
-    ],
-    [
-        'text' => 'Ritirare i cappotti dalla lavanderia',
-        'isDone' => false,
-        'id' => 1,
-    ],
-    [
-        'text' => 'Tagliare il prato',
-        'isDone' => false,
-        'id' => 2,
-    ],
-    [
-        'text' => 'Fare le pulizie',
-        'isDone' => false,
-        'id' => 3,
-    ],
-    [
-        'text' => 'Allenarsi',
-        'isDone' => false,
-        'id' => 4,
-    ],
-];
-
 // Get Value from frontend
 $new_task = $_POST['newTask'] ?? false;
 

f9f8affb3a0ca0b93f37464b6bc02b34fa25bd8b
marcocareddu <saldumaccu@tuta.io>
2023-07-20 14:02:44 +0200

    PHP JS - Moved Tasks from JS to PHP

--- api/todo/index.php
+++ api/todo/index.php
@@ -1,1 +1,30 @@
 <?php
+
+// Moved array from JS
+$tasks = [
+    [
+        'text' => 'Comprare il pane',
+        'isDone' => false,
+        'id' => 0,
+    ],
+    [
+        'text' => 'Ritirare i cappotti dalla lavanderia',
+        'isDone' => false,
+        'id' => 1,
+    ],
+    [
+        'text' => 'Tagliare il prato',
+        'isDone' => false,
+        'id' => 2,
+    ],
+    [
+        'text' => 'Fare le pulizie',
+        'isDone' => false,
+        'id' => 3,
+    ],
+    [
+        'text' => 'Allenarsi',
+        'isDone' => false,
+        'id' => 4,
+    ],
+];
--- js/script.js
+++ js/script.js
@@ -7,33 +7,6 @@ const app = createApp({
     data() {
         return {
             newTask: '',
-            tasks: [
-                {
-                    text: 'Comprare il pane',
-                    isDone: false,
-                    id: 0,
-                },
-                {
-                    text: 'Ritirare i cappotti dalla lavanderia',
-                    isDone: false,
-                    id: 1,
-                },
-                {
-                    text: 'Tagliare il prato',
-                    isDone: false,
-                    id: 2,
-                },
-                {
-                    text: 'Fare le pulizie',
-                    isDone: false,
-                    id: 3,
-                },
-                {
-                    text: 'Allenarsi',
-                    isDone: false,
-                    id: 4,
-                },
-            ]
         }
     },
     methods: {

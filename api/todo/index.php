576976c656d2f294d3298ce809d6adb1ea0d7081
marcocareddu <saldumaccu@tuta.io>
2023-07-20 15:37:55 +0200

    PHP - Add new task permanently

--- api/todo/index.php
+++ api/todo/index.php
@@ -1,11 +1,22 @@
 <?php
+// Link to Json file
+$json_path = __DIR__ . '../data/tasks.json';
+$json_tasks = file_get_contents($json_path);
+
+$tasks = json_decode($json_tasks, true);
 
 // Get Value from frontend
-$new_task = $_POST['newTask'] ?? false;
+$new_task = $_POST['newTask'] ?? null;
 
 // Add new task
 if ($new_task) {
     $tasks[] = $new_task;
+
+    // Encode json
+    $task_encoded = json_encode($tasks);
+
+    // Put newtask into file
+    file_put_contents($json_path, $task_encoded);
 };
 
 // Create Json API response
